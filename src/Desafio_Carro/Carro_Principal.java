69e5570d9851193194f21eb2adeb88e32dde01b5
GuilhermeRoecker <guilherme.roecker@outlook.com.br>
2023-08-24 18:54:36 -0300

    Corrigido Identação

--- src/Desafio_Carro/Carro_Principal.java
+++ src/Desafio_Carro/Carro_Principal.java
@@ -1,99 +1,101 @@
 package Desafio_Carro;
+
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Carro_Principal {
     public static void main(String[] args) {
         ArrayList<Carro> carros = new ArrayList<Carro>();
-        String menu = "1 - Cadastrar \n" 
-        + "2 - Listar Todos os carros \n"
-        + "3 - Lista por cor \n"
-        + "4 - Lista por Marca \n" 
-        + "5 - Lista por Ano \n"
-        + "6 - Sair";
+        String menu = "1 - Cadastrar \n"
+                + "2 - Listar Todos os carros \n"
+                + "3 - Lista por cor \n"
+                + "4 - Lista por Marca \n"
+                + "5 - Lista por Ano \n"
+                + "6 - Sair";
 
         int op = 0;
-        do{
-            op = Integer.parseInt(JOptionPane.showInputDialog(null, menu ));
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(null, menu));
 
             // Cadastro de carro
-            if (op == 1){
+            if (op == 1) {
                 Carro c = new Carro();
                 c.Cadasto();
                 carros.add(c);
             }
 
             // Listagem de carros cadastrados
-            if (op == 2 ){
+            if (op == 2) {
                 String todos = "Carros cadastrados \n \n";
-                for (Carro c: carros){
+                for (Carro c : carros) {
                     todos += c.exibirCarros();
                 }
-                JOptionPane.showMessageDialog(null,todos);
+                JOptionPane.showMessageDialog(null, todos);
             }
 
             // Pesquisa por cor
-            if (op == 3){
-              String selecionarCor = JOptionPane.showInputDialog(null, "Qual a cor?");
-              String corSelecionada = " ";
-              Boolean temcor = false;
+            if (op == 3) {
+                String selecionarCor = JOptionPane.showInputDialog(null, "Qual a cor?");
+                String corSelecionada = " ";
+                Boolean temcor = false;
 
-              for (Carro c: carros){
+                for (Carro c : carros) {
 
-                //Verifica se possui carro cadastrado conforme criterio
-                if(selecionarCor.equalsIgnoreCase(c.cor)){
-                corSelecionada += c.exibirCarros();
-                temcor = true;
+                    // Verifica se possui carro cadastrado conforme criterio
+                    if (selecionarCor.equalsIgnoreCase(c.cor)) {
+                        corSelecionada += c.exibirCarros();
+                        temcor = true;
                     }
-                // Tomada de decisão dependendp do paramentro passado pelo usuario    
-                } if(!temcor){
-                      JOptionPane.showMessageDialog(null, "Não possui nenhum carro com essa cor");
-                  }else{
-                      JOptionPane.showMessageDialog(null, corSelecionada);
-                  }
+                    // Tomada de decisão dependendp do paramentro passado pelo usuario
+                }
+                if (!temcor) {
+                    JOptionPane.showMessageDialog(null, "Não possui nenhum carro com essa cor");
+                } else {
+                    JOptionPane.showMessageDialog(null, corSelecionada);
+                }
             }
 
             // Pesquisa por Marca
-            if (op == 4){
-             
+            if (op == 4) {
+
                 String selecionaMarca = JOptionPane.showInputDialog(null, "Qual a marca?");
                 String MarcaSelecionada = " ";
-                Boolean temMarca =false;
+                Boolean temMarca = false;
 
-                for (Carro c1: carros){
-   
-                 //Verifica se possui carro cadastrado conforme criterio
-                 if(selecionaMarca.equalsIgnoreCase(c1.fabricante)){
-                 MarcaSelecionada += c1.exibirCarros();
-                 temMarca =true;  
+                for (Carro c1 : carros) {
+
+                    // Verifica se possui carro cadastrado conforme criterio
+                    if (selecionaMarca.equalsIgnoreCase(c1.fabricante)) {
+                        MarcaSelecionada += c1.exibirCarros();
+                        temMarca = true;
+                    }
                 }
-              }
 
-               // Tomada de decisão dependendp do paramentro passado pelo usuario
-               if(!temMarca){
-                JOptionPane.showMessageDialog(null, "Não possui nenhum carro com essa marca");
-               } else{
-                 JOptionPane.showMessageDialog(null, MarcaSelecionada);
+                // Tomada de decisão dependendp do paramentro passado pelo usuario
+                if (!temMarca) {
+                    JOptionPane.showMessageDialog(null, "Não possui nenhum carro com essa marca");
+                } else {
+                    JOptionPane.showMessageDialog(null, MarcaSelecionada);
                 }
             }
-            
+
             // Pesquisa por periodo de ano
-            if(op == 5){
-                int anoIni = Integer.parseInt(JOptionPane.showInputDialog(null , "Qual o ano Inicial?"));
-                int anoFin = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual o ano final?"));                
+            if (op == 5) {
+                int anoIni = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual o ano Inicial?"));
+                int anoFin = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual o ano final?"));
                 String resultado = "";
                 boolean temAno = false;
-                for(Carro c2: carros){
-                    if(c2.ano >= anoIni && c2.ano  <= anoFin ){                        
-                    resultado += c2.exibirCarros();   
-                    temAno= true;                         
-                }
+                for (Carro c2 : carros) {
+                    if (c2.ano >= anoIni && c2.ano <= anoFin) {
+                        resultado += c2.exibirCarros();
+                        temAno = true;
+                    }
 
-              }                // Tomada de decisão dependendp do paramentro passado pelo usuario
-               if(!temAno){
-                JOptionPane.showMessageDialog(null, "Não possui nenhum carro nesse periodo de ano");
-               } else{
-                 JOptionPane.showMessageDialog(null, resultado);
+                } // Tomada de decisão dependendp do paramentro passado pelo usuario
+                if (!temAno) {
+                    JOptionPane.showMessageDialog(null, "Não possui nenhum carro nesse periodo de ano");
+                } else {
+                    JOptionPane.showMessageDialog(null, resultado);
                 }
             }
 
